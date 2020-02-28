da3354df1adc1cdc7a132112d39664f4adcfb0cb
zhuo.peng <zhuo.peng@tcl.com>
2020-02-28 09:50:26 +0800

    Fri Feb 28 09:50:26 CST 2020 --> update linux/09_ssh_reverse/.startd.sh

--- linux/09_ssh_reverse/.startd.sh
+++ linux/09_ssh_reverse/.startd.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+count=0
 while true; do
-    RET=`ps ax | grep "ssh -f -N -R 8868:localhost:22" | grep -v "grep"`
-    if [ "$RET" = "" ]; then
-        echo "restart ssh server"
+    PID=`ps ax | grep "8868:localhost:22" | grep -v "grep" | cut -d " " -f 1`
+    if [ "$PID" = "" ]; then
+        count=0
+        echo "`date` --> restart 8868"
         ssh -f -N -R 8868:localhost:22 ubuntu@tx.jjmoo.com
+    else
+        let count++
+        if [ $count -ge 30 ]; then
+            count=0
+            RET=`who -a | grep "127.0.0.1"`
+            if [ "$RET" = "" ]; then
+                echo "`date` --> kill 8868"
+                kill $PID
+            fi
+        fi
     fi
     sleep 10
 done
