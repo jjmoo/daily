5d839d8c3587f8fa2ac1971c72e656df2d38b0d5
Zohn PENG <pengzhuo1993@foxmail.com>
2020-02-29 22:22:56 +0800

    Sat Feb 29 22:22:56 CST 2020 --> leetcode/0123_Best_Time_to_Buy_and_Sell_Stock_III.py

--- leetcode/0123_Best_Time_to_Buy_and_Sell_Stock_III.py
+++ leetcode/0123_Best_Time_to_Buy_and_Sell_Stock_III.py
@@ -48,9 +48,35 @@ class Solution(object):
                         pass
         return tr1[2] + tr2[2]
 
+    def maxProfit2(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices:
+            return 0
+        size = len(prices)
+        left, right = [0] * size, [0] * size
+        min_left, max_right = prices[0], prices[-1]
+        for i in range(1, size):
+            left[i] = max(left[i - 1], prices[i] - min_left)
+            min_left = min(min_left, prices[i])
+            right[- i - 1] = max(right[-i], max_right - prices[- i - 1])
+            max_right = max(max_right, prices[- i - 1])
+        profit = 0
+        for i in range(size):
+            profit = max(profit, left[i] + right[i])
+        return profit
+
 
 print(Solution().maxProfit([3,3,5,0,0,3,1,4])) # 6
 print(Solution().maxProfit([1,2,3,4,5])) # 4
 print(Solution().maxProfit([7,6,4,3,1])) # 0
 print(Solution().maxProfit([2,1,4])) # 3
 print(Solution().maxProfit([6,1,3,2,4,7])) # 7
+
+print(Solution().maxProfit2([3,3,5,0,0,3,1,4])) # 6
+print(Solution().maxProfit2([1,2,3,4,5])) # 4
+print(Solution().maxProfit2([7,6,4,3,1])) # 0
+print(Solution().maxProfit2([2,1,4])) # 3
+print(Solution().maxProfit2([6,1,3,2,4,7])) # 7
