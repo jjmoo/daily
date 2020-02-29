dadf53442b1a47e3736769918b9317ec9aeb660d
zhuo.peng <zhuo.peng@tcl.com>
2020-02-29 18:11:01 +0800

    Sat Feb 29 18:11:01 CST 2020 --> linux/09_ssh_reverse/.startd.sh

--- linux/09_ssh_reverse/.startd.sh
+++ linux/09_ssh_reverse/.startd.sh
@@ -2,7 +2,7 @@
 
 count=0
 while true; do
-    PID=`ps ax | grep "8868:localhost:22" | grep -v "grep" | cut -d " " -f 1`
+    PID=`ps ax | grep "8868:localhost:22" | grep -v "grep" | awk '{print $1}'`
     if [ "$PID" = "" ]; then
         count=0
         echo "`date` --> restart 8868"
