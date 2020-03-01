df50acd8b3a6067b881148d2756cc2cc2a471457
Zohn PENG <pengzhuo1993@foxmail.com>
2020-03-01 16:57:04 +0800

    Sun Mar  1 16:57:04 CST 2020 --> leetcode/T0188_Best_Time_to_Buy_and_Sell_Stock_IV.py

---
+++ leetcode/utils.py
@@ -0,0 +1,12 @@
+import time
+
+class Benchmark():
+    """Benchmark programs."""
+    def __init__(self, prefix=None):
+        self.prefix = prefix + ' ' if prefix else ''
+
+    def __enter__(self):
+        self.start = time.time()
+
+    def __exit__(self, *args):
+        print('%stime: %.4f sec' % (self.prefix, time.time() - self.start))
