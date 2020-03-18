cfc11dffda84a6f0e4d512cf1a8be7e030f1acbb
zhuo.peng <zhuo.peng@tcl.com>
2020-03-18 16:34:20 +0800

    Wed Mar 18 16:34:20 CST 2020 --> leetcode/M0146_LRU_Cache.py

--- leetcode/M0146_LRU_Cache.py
+++ leetcode/M0146_LRU_Cache.py
@@ -1,9 +1,10 @@
 class DequeNode:
-    def __init__(self, k, x):
+    def __init__(self, k = 0, x = 0):
         self.key = k
         self.val = x
-        self.past = None
+        self.prev = None
         self.next = None
+
 
 class LRUCache:
     def __init__(self, capacity: int):
@@ -33,43 +34,101 @@ class LRUCache:
     def __insert(self, node: DequeNode):
         if self.head:
             node.next = self.head
-            self.head.past = node
+            self.head.prev = node
         self.head = node
         if not self.tail: self.tail = node
 
     def __update(self, node: DequeNode):
         if node is self.head: return
-        node.past.next = node.next
-        if node is self.tail: self.tail = node.past
-        else: node.next.past = node.past
-        node.past = None
+        node.prev.next = node.next
+        if node is self.tail: self.tail = node.prev
+        else: node.next.prev = node.prev
+        node.prev = None
         self.__insert(node)
 
     def __pop(self):
         node = self.tail
         if node is self.head: self.head, self.tail = None, None
-        else: self.tail, node.past.next = node.past, None
+        else: self.tail, node.prev.next = node.prev, None
         return node
 
 
-# cache = LRUCache(2)
-# cache.put(1, 1)
-# cache.put(2, 2)
-# print(1, cache.get(1))
-# cache.put(3, 3)
-# print(-1, cache.get(2))
-# cache.put(4, 4)
-# print(-1, cache.get(1))
-# print(3, cache.get(3))
-# print(4, cache.get(4))
+class LRUCacheGuard:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.size = 0
+        self.pool = {}
+        self.head = DequeNode()
+        self.head.next = self.head
+        self.head.prev = self.head
 
-cache = LRUCache(2)
+    def get(self, key: int) -> int:
+        if key in self.pool:
+            node = self.pool[key]
+            self._move_to_head(node)
+            return node.val
+        else: return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.pool:
+            self.pool[key].val = value
+            self._move_to_head(self.pool[key])
+        else:
+            node = DequeNode(key, value)
+            self.pool[key] = node
+            self._add_node(node)
+            if self.size < self.capacity: self.size += 1
+            else: self.pool.pop(self._pop_tail().key)
+
+    def _add_node(self, node):
+        self.head.next.prev, node.next = node, self.head.next
+        self.head.next, node.prev = node, self.head
+
+    def _remove_node(self, node):
+        node.next.prev, node.prev.next = node.prev, node.next
+        return node
+
+    def _move_to_head(self, node):
+        self._remove_node(node)
+        self._add_node(node)
+
+    def _pop_tail(self):
+        return self._remove_node(self.head.prev)
+
+
+from collections import OrderedDict
+
+class LRUCacheOrderedDict(OrderedDict):
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self: return -1
+        self.move_to_end(key)
+        return self[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self: self.move_to_end(key)
+        self[key] = value
+        if len(self) > self.capacity: self.popitem(last=False)
+
+
+cache = LRUCacheOrderedDict(2)
+cache.put(1, 1)
+cache.put(2, 2)
+print(1, cache.get(1))
+cache.put(3, 3)
+print(-1, cache.get(2))
+cache.put(4, 4)
+print(-1, cache.get(1))
+print(3, cache.get(3))
+print(4, cache.get(4))
 cache.put(2, 1)
 cache.put(2, 2)
-print(cache.get(2))
+print(2, cache.get(2))
 cache.put(1, 1)
 cache.put(4, 1)
-print(cache.get(2))
+print(-1, cache.get(2))
 
 
 # Your LRUCache object will be instantiated and called as such:
